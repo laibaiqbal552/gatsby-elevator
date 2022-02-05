8ab761818a676b96cec1361fe19c43b36c173170
hamza <hamzaliaqat1481@gmail.com>
2022-02-05 20:25:01 +0500

    latest fixes

---
+++ src/pages/software-developer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Index from "../views/softwareDeveloper/Index";
+
+function softwareDeveloper() {
+  return (
+    <div>
+      <Index />
+    </div>
+  );
+}
+
+export default softwareDeveloper;
